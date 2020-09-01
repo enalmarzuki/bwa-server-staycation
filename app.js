cf6f79b0ed1cd87aec3aacc7ebd3667976197d46
enalmarzuki <40693134+enalmarzuki@users.noreply.github.com>
2020-09-01 19:53:27 +0800

    Deploy Heroku

--- app.js
+++ app.js
@@ -17,12 +17,15 @@ const flash = require("connect-flash");
 
 // import mongoose
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/db_staycation", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  "mongodb+srv://enaldev:bwamern@cluster0.henup.mongodb.net/db_staycation?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
